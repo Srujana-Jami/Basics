0aa96b0d18d5d3ec68200a7f840d3f0fc75564ed
Srujana-Jami <srujana.jami@innroad.com>
2020-07-27 16:48:06 +0530

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Enter first no");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second no"); 
+            b = Convert.ToInt32(Console.ReadLine());
+            c = a + b;
+            Console.WriteLine("Result:" + c);
+            Console.ReadLine();
+           
+        }
+    }
+}
